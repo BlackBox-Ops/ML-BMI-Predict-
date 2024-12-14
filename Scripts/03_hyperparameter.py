673ad2e18c52ddc2b3c082b4db9f9cc6081b1b70
BlackBox-Ops <edukasitime@gmail.com>
2024-12-14 21:20:27 +0700

    melakukan testing pada code program hyperparameter.py untuk mengetahui paramter yang cocok pada model yang kita kembangkan

--- Scripts/03_hyperparameter.py
+++ Scripts/03_hyperparameter.py
@@ -28,7 +28,7 @@ class RandomForestTuner:
     def select_features(self, data):
         # Load fitur x dan y dari datasheet yang akan digunakan 
         X = data[self.feature_columns]
-        y = data[self.target_column]
+        y = data[self.target_columns]
         return X, y 
     
     # Buat Fungsi untuk tune hyperparameter model
@@ -38,7 +38,7 @@ class RandomForestTuner:
             'n_estimators' : [50, 100, 200],
             'max_depth' : [None, 10, 20, 30],
             'min_samples_split' : [2, 5, 10],
-            'min_smaples_leaf' : [1,2,4], 
+            'min_samples_leaf' : [1, 2, 4], 
         }
 
         rf = RandomForestClassifier(random_state=42)                # Load model Random Forest Classifier
@@ -52,5 +52,51 @@ class RandomForestTuner:
         self.best_model = grid_search.best_estimator_ 
         # tampilkan parameter terbaik 
         print(f'Best Parameters : {grid_search.best_params_}')
+    
+    # Buat Fungsi untuk evaluasi hasil tunning hyperparameter 
+    def evaluate_model(self, X, y, dataset_name):
+        # Evaluasi model datasheet dan tampilhan hasil evaluasi 
+        predictions = self.best_model.predict(X)  # Prediksi model untuk variabel x (features)
+        accuracy = accuracy_score(y, predictions) # Hitung akurasi dari hasil tunning hyperparameter 
+        report = classification_report(y, predictions)
 
+        print(f'{dataset_name} accuracy : {accuracy : .4f}') # tampilkan hasil dari akurasi hyperparameter 
+        print(f'{dataset_name} accuracy : :\n {report}')     # tampilkan hasil dari report pelatihan 
+    
+    # Buat Fungsi untuk menjalankan program 
+    def run(self):
+        '''
+        Main execution flow for loading data training, and evaluation 
+        '''
+
+        # Load data 
+        print("Loading data ....")
+        self.load_data()
+
+        # Prepare datasheet 
+        print("Preparing datasheet ....")
+        X_train, y_train = self.select_features(self.train_data)
+        X_valid, y_valid = self.select_features(self.valid_data)
+        X_test,  y_test = self.select_features(self.test_data)
+
+        # Perform hyperparameter tuning 
+        print("Tuning Hyperparameters ...")
+        self.tune_hyperparameter(X_train, y_train) # train model using best hyperparameter random forest algorithm 
+
+        # Evaluasi model dan test validation data 
+        print("Evaluating Model ...")
+        self.evaluate_model(X_valid, y_valid, "Validation Data") # validasi model 
+        self.evaluate_model(X_test, y_test, "Test Data") # validasi model 
+
+# Main Execution 
+if __name__ == '__main__':
+    # configuration 
+
+    DATA_DIR = 'Data'
+    FEATURE_COLUMNS = ['Gender','Height','Weight']
+    TARGET_COLUMNS = 'Index'
+
+    # initialize and run the random tuner 
+    tuner = RandomForestTuner(DATA_DIR, FEATURE_COLUMNS, TARGET_COLUMNS)
+    tuner.run()
         
